652afd503316c95b6e61632ac4ed01498158c70b
성문 <castledoor@pusan.ac.kr>
2024-04-14 11:41:51 +0900

    정렬 조건이 복잡하거나, 사용자 정의 자료형을 정렬할 때 'comp'를 직접 작성 ***
    https://en.cppreference.com/w/cpp/named_req/Compare

--- improvements/람다 응용 정렬.cpp
+++ improvements/람다 응용 정렬.cpp
@@ -15,7 +15,7 @@ int main() {
 
     vector<PairInt> vpii{a, b, c};
 
-    // std::sort(
+    // std::sort(first, last, comp);
     sort(vpii.begin(), vpii.end(), [](const auto& a, const auto& b)->bool{ // 지금처럼 컴파일러가 리턴 타입을 추론할 수 있는 경우 `->{return type}`은 생략 가능
         if (a.second == b.second) return a.first < b.first; // second가 같을 경우 first의 오름차순으로 정렬
         else return a.second < b.second; // second가 다를 경우 second의 오름차순으로 정렬
