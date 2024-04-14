3d934d860007bae74f0f329722ab5105545bcfc7
성문 <castledoor@pusan.ac.kr>
2024-04-14 11:40:36 +0900

    정렬 조건이 복잡하거나, 사용자 정의 자료형을 정렬할 때 'comp'를 직접 작성 ***
    https://en.cppreference.com/w/cpp/named_req/Compare

---
+++ improvements/람다 응용 정렬.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct PairInt{ // C++의 `struct`는 C에서의 `struct`와는 많이 다르지만..
+    int first;
+    int second;
+};
+
+int main() {
+    PairInt a = PairInt{3, 4};
+    auto b = PairInt{9999, 2};
+    PairInt c{5, 2};
+
+    vector<PairInt> vpii{a, b, c};
+
+    // std::sort(
+    sort(vpii.begin(), vpii.end(), [](const auto& a, const auto& b)->bool{ // 지금처럼 컴파일러가 리턴 타입을 추론할 수 있는 경우 `->{return type}`은 생략 가능
+        if (a.second == b.second) return a.first < b.first; // second가 같을 경우 first의 오름차순으로 정렬
+        else return a.second < b.second; // second가 다를 경우 second의 오름차순으로 정렬
+    });
+    // 기본 자료형이 아니기 때문에 sort(vpii.begin(), vpii.end()); 처럼은 사용할 수 없다!
+
+    for (const auto& it : vpii) cout << it.first << ' ' << it.second << '\n';
+}
