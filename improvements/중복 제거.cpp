fa254731860619c17a7b9034eaba08b53b10ee5c
성문 <castledoor@pusan.ac.kr>
2024-05-08 20:00:44 +0900

    (C++20) std::ranges::unique() 활용하기 *
    C++20 이전 버전에서 사용 가능한 코드 포함 ( 주석 )

---
+++ improvements/중복 제거.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <ranges> // C++20
+
+int main() {
+    std::vector<int> vi{3, 6, 6, 7, 9, 9, 8, 3, 6};
+
+    std::ranges::sort(vi); // unique() 사용 전에는 반드시 컨테이너를 정렬해줘야 함!
+    auto [fr, rr] = std::ranges::unique(vi); // `std::ranges::unique()`는 `std::unique()`같은 작업을 수행한 다음 `std::ranges::subrange` 객체를 리턴함! `std::ranges::subrange`는 `.begin()`, `.end()`로 반환되는 이터레이터 쌍에 대한 래퍼(wrapper)!
+    vi.erase(fr, rr); // .erase()는 벡터 컨테이너의 메소드이기 때문에  `using namespace std;`가 없어도 `std::` prefix가 붙지 않는다.
+    for (const auto& it : vi) std::cout << it << '\n';
+
+/*
+ * until C++17:
+
+    std::sort(vi.begin(), vi.end());
+    vi.erase(std::unique(vi.begin(), vi.end()), vi.end()); // erase-remove 관용구와 비슷한 형태의 erase-unique 관용구
+    // for (size_t i = 0; i < vi.size(); i++) std::cout << vi[i] << '\n'; // C++11 이전 버전에서도 사용 가능한 일반 반복문
+    for (const auto& it : vi) std::cout << it << '\n';
+ */
+}
