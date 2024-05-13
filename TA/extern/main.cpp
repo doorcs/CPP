fae47f4af25768cd07807334d0aa5d1fcc130eb7
성문 <castledoor@pusan.ac.kr>
2024-05-14 00:31:55 +0900

    extern 키워드로 다른 파일에서 정의된 변수와 함수 가져오기

---
+++ TA/extern/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+// extern {자료형} 변수명;
+extern std::vector<int> vi; // 다른 소스 파일(헤더XX)에서 정의된 전역 변수를 `선언`
+
+// 함수는 묵시적으로 extern이기 때문에 extern을 생략할 수 있습니다:
+bool isVectorEmpty(std::vector<int>& vec); // 다른 소스파일에서 정의된 함수를 `선언`
+
+// extern bool ret; 지역변수는 extern 키워드로 가져올 수 없습니다!
+
+int main() {
+		for(auto it: vi) std::cout << it << ' '; // main.cpp에서는 벡터를 선언
+
+		if(!isVectorEmpty(vi)) std::cout << '\n' << "This vector is NOT empty";
+
+		// std::cout << '\n' << ret; 지역변수는 extern 키워드로 가져올 수 없습니다!
+}
---
+++ TA/extern/readme.md
@@ -0,0 +1,5 @@
+## `main.cpp`, `data.cpp`를 함께 컴파일해줘야 합니다!
+> g++ -std=c++17 main.cpp data.cpp -o a
+>
+> `-std=c++17`: C++17 표준으로 컴파일
+> `-o a`: output file 이름을 a로
