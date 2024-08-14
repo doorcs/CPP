24a01cb78b0565930565f29f44222cf6d2c6bbf0
성문 <castledoor@pusan.ac.kr>
2024-08-14 16:10:39 +0900

    for문으로 n개의 원소를 벡터에 채우기 *
    아름다움은 주관적이니깐..

--- improvements/벡터 채우기.cpp
+++ improvements/벡터 채우기.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+  int N;
+  cin >> N;
 
-    // 처음 떠올렸던 방법
-    vector<int> v1;
+  // 가장 단순한 방법:
+  vector<int> v1;
+  for (int i = 0; i < N; i++) {
+      int tmp;
+      cin >> tmp;
+      v1.push_back(tmp);
+  }
 
-    for(int i=0; i<n; i++) {
-        int tmp;
-        cin >> tmp;
-        v1.push_back(tmp);
-    }
+  // 더 효율적인 방법:
+  vector<int> v2(N);
+  for (int i = 0; i < N; i++) cin >> v2[i];
 
-    // 더 효율적인 방법
-    vector<int> v2(n);
+  // 아름다운 방법 (since C++11):
+  vector<int> v3(N);
+  for (auto& it : v3) cin >> it;
 
-    for(int i=0; i<n; i++) cin >> v2[i];
-
+  // for (int i = 0; i < N; i++) cout << vec[i] << ' ';
+  // for (const auto& it : vec) cout << it << ' '; // primitive type은 복사 비용이 낮기 때문에 `for (int it : vec)` 처럼 써도 무방함
 }
